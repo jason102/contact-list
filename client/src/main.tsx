0c5fdde4cdef92583d0d473d8d008d0472dde92e
Jason Stine <lostlaowai@outlook.com>
2023-11-14 23:44:41 -0800

    feat: add react-router-dom

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,17 @@ import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import store from "./app/store";
 import theme from "./theme";
-import App from "./App.tsx";
+import router from "src/app/clientRouter.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
